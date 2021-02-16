531fbe4d15a11bceab00a011d722f9c10de2e128
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2021-02-16 17:48:56 +0800

    initial

---
+++ Services/OddsEventKafkaReactor.php
@@ -0,0 +1,264 @@
+<?php
+
+use RdKafka\KafkaConsumer;
+use Smf\ConnectionPool\ConnectionPool;
+use Smf\ConnectionPool\Connectors\CoroutinePostgreSQLConnector;
+use Swoole\Coroutine\PostgreSQL;
+
+require_once __DIR__ . '/../Bootstrap.php';
+
+function makeConsumer() {
+    global $kafkaConf;
+
+    $kafkaConsumer = new KafkaConsumer($kafkaConf);
+    $kafkaConsumer->subscribe([
+        getenv('KAFKA-SCRAPING-ODDS', 'SCRAPING-ODDS'), 
+        getenv('KAFKA-SCRAPING-EVENTS', 'SCRAPING-PROVIDER-EVENTS')
+    ]);
+
+    echo "Setting Up Odds & Events Scraping Consumer";
+
+	return $kafkaConsumer;
+}
+
+// function databaseConnectionPool()
+// {
+//     $pool = new ConnectionPool(
+//         [
+//             'minActive'         => 10,
+//             'maxActive'         => 30,
+//             'maxWaitTime'       => 5,
+//             'maxIdleTime'       => 20,
+//             'idleCheckInterval' => 10,
+//         ],
+//         new CoroutinePostgreSQLConnector,
+//         [
+//             'connection_strings' => "host=" . getenv('DB_HOST', 'db') . " port=" . getenv('DB_PORT', 5432) . " dbname=" . getenv('DB_DATABASE', 'multilinev2') . " user=" . getenv('DB_USERNAME', 'postgres') . " password=" . getenv('DB_PASSWORD', 'password'),
+//         ]
+//     );
+    
+
+//     $pool->init();
+
+//     $connection = $pool->borrow();
+//     $status = $connection->query('SELECT id FROM users');
+//     $stat = $connection->fetchAssoc($status);
+    
+
+//     echo "Return the connection to pool as soon as possible\n";
+//     $pool->return($connection);
+
+//     var_dump($stat);
+// }
+
+function reactor($queue, &$dbPool) {
+	global $count;
+    global $activeProcesses;
+	while (true) {
+		$message = $queue->consume(0);
+		if ($message) {
+			switch ($message->err) {
+				case RD_KAFKA_RESP_ERR_NO_ERROR:
+					if ($message->payload) {
+                        $key = getPipe(getenv('ODDS-EVENTS-PROCESSES-NUMBER', 1));
+
+                        $payload = json_decode($message->payload, true);
+                        switch($payload['command']) {
+                            case 'odd':
+                                // go("oddHandler", $key, $payload, $message->offset);
+                                oddHandler($key, $dbPool, $payload, $message->offset);
+                                break;
+                            case 'event':
+                            default:
+                                // go("eventHandler", $key, $payload, $message->offset);
+                                eventHandler($key, $payload, $message->offset);
+                                break;
+                        }
+						
+						$activeProcesses++;
+						$count++;
+                    }
+                    $queue->commitAsync($message);
+					break;
+				case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+					echo "No more messages; will wait for more\n";
+					break;
+				case RD_KAFKA_RESP_ERR__TIMED_OUT:
+					// echo "Timed out\n";
+					break;
+				default:
+					throw new Exception($message->errstr(), $message->err);
+					break;
+			}
+		} else {
+			Co\System::sleep(0.001);
+		}
+	}
+}
+
+function oddHandler($key, &$dbPool, $message, $offset)
+{
+    global $swooleTable;
+
+    $start = microtime(true);
+
+    try {
+
+        // $previousTS = $swooleTable['timestamps']['odds']['ts'];
+        // $messageTS  = $message["request_ts"];
+
+        // if ($messageTS < $previousTS) {
+        //     $statsArray = [
+        //         "type"        => "odds",
+        //         // "status"      => $swooleStats::getErrorType('TIMESTAMP_ERROR'),
+        //         "time"        => microtime(true) - $start,
+        //         "request_uid" => $message["request_uid"],
+        //         "request_ts"  => $message["request_ts"],
+        //         "offset"      => $offset,
+        //     ];
+
+        //     // SwooleStats::addStat($statsArray);
+        //     freeUpProcess();
+        //     return;
+        // }
+
+        // if (!is_array($message["data"]) || empty($message["data"])) {
+        //     $statsArray = [
+        //         "type"        => "odds",
+        //         // "status"      => SwooleStats::getErrorType('ERROR'),
+        //         "time"        => microtime(true) - $start,
+        //         "request_uid" => $message["request_uid"],
+        //         "request_ts"  => $message["request_ts"],
+        //         "offset"      => $offset,
+        //     ];
+        //     // SwooleStats::addStat($statsArray);
+        //     freeUpProcess();
+        //     return;
+        // }
+
+        // $toHashMessage = $message["data"];
+        // unset($toHashMessage['runningtime'], $toHashMessage['id']);
+        // $caching = 'odds-' . md5(json_encode((array) $toHashMessage));
+
+        // if (!empty($toHashMessage['events'][0]['eventId'])) {
+        //     $eventId = $toHashMessage['events'][0]['eventId'];
+
+        //     if ($swooleTable['eventOddsHash']->exists($eventId) && $swooleTable['eventOddsHash'][$eventId]['hash'] == $caching) {
+        //         $statsArray = [
+        //             "type"        => "odds",
+        //             // "status"      => SwooleStats::getErrorType('HASH_ERROR'),
+        //             "time"        => microtime(true) - $start,
+        //             "request_uid" => $message["request_uid"],
+        //             "request_ts"  => $message["request_ts"],
+        //             "offset"      => $offset,
+        //         ];
+        //         // SwooleStats::addStat($statsArray);
+        //         freeUpProcess();
+        //         return;
+        //     }
+        // }
+        // $swooleTable['eventOddsHash'][$eventId] = ['hash' => $caching];
+
+        // if (!$swooleTable['enabledSports']->exists($message["data"]["sport"])) {
+        //     $statsArray = [
+        //         "type"        => "odds",
+        //         // "status"      => SwooleStats::getErrorType('EVENTS_INACTIVE_SPORT'),
+        //         "time"        => microtime(true) - $start,
+        //         "request_uid" => $message["request_uid"],
+        //         "request_ts"  => $message["request_ts"],
+        //         "offset"      => $offset,
+        //     ];
+        //     // SwooleStats::addStat($statsArray);
+        //     freeUpProcess();
+        //     return;
+        // }
+
+        // if (!$$swooleTable['enabledProvidersTable']->exists($message["data"]["provider"])) {
+        //     $statsArray = [
+        //         "type"        => "odds",
+        //         "status"      => SwooleStats::getErrorType('EVENTS_INACTIVE_PROVIDER'),
+        //         "time"        => microtime(true) - $start,
+        //         "request_uid" => $message["request_uid"],
+        //         "request_ts"  => $message["request_ts"],
+        //         "offset"      => $offset,
+        //     ];
+        //     // SwooleStats::addStat($statsArray);
+        //     freeUpProcess();
+        //     return;
+        // }
+
+        echo 1;
+        go(function() use($dbPool) {
+            echo 2;
+            try {
+
+                echo 3;
+                // $dbPool->init();
+                echo 4;
+                $connection = $dbPool->borrow();
+                var_dump($connection);
+                echo 5;
+                $result = $connection->query("SELECT id FROM users");
+                echo 6;
+                $stat = $connection->fetchAssoc($result);
+                $pool->return($connection);
+                echo 7;
+                var_dump($stat);
+            } catch (Exception $e) {
+                echo 9;
+                echo $e->getMessage();
+            }
+            
+            // ProcessOdds
+        });
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        // self::$configTable["processKafka"]["value"] = 0;
+        // processTaskTempDir(false);
+        // self::$configTable["processKafka"]["value"] = 1;
+    } finally {
+        freeUpProcess();
+        return true;
+    }
+}
+
+function eventHandler($payload, $key)
+{
+    freeUpProcess();
+}
+
+function execute($payload,$key) {
+	#var_dump($payload);
+	#var_dump($key);
+	$myPayload=json_decode($payload);
+	if($myPayload) {
+		Co\System::sleep(0.025);
+		echo "c";
+		freeUpProcess();
+	}
+	else {
+		echo "x";
+	}
+}
+
+$activeProcesses = 0;
+$queue           = makeConsumer();
+$dbPool = null;
+makeProcess();
+
+Co\run(function() use ($queue, $activeProcesses) {
+    global $dbPool;
+
+	$count = 0;
+
+    Swoole\Timer::tick(1000, "checkRate");
+    $dbPool = databaseConnectionPool();
+
+    $dbPool->init();
+    defer(function () use ($dbPool) {
+        echo "Closing connection pool\n";
+        $dbPool->close();
+    });
+
+    reactor($queue, $dbPool);
+});
---
+++ config.php
@@ -0,0 +1,327 @@
+<?php
+
+$config = [
+    'database' => [
+        'connection_pool' => [
+            'minActive'         => 10,
+            'maxActive'         => 30,
+            'maxWaitTime'       => 5,
+            'maxIdleTime'       => 20,
+            'idleCheckInterval' => 10,
+        ],
+        'pgsql' => [
+            'host'        => getenv('DB_HOST', '127.0.0.1'),
+            'port'        => getenv('DB_PORT', 5432),
+            'dbname'      => getenv('DB_DATABASE', 'multilinev2'),
+            'user'        => getenv('DB_USERNAME', 'postgres'),
+            'password'    => getenv('DB_PASSWORD', 'password')
+        ]
+    ],
+    'swoole_tables'            => [
+        'config'                    => [
+            'size'   => 2,
+            'column' => [
+                ['name' => 'value', 'type' => \Swoole\Table::TYPE_FLOAT],
+            ],
+        ],
+        'timestamps'                => [
+            'size'   => 2,
+            'column' => [
+                ['name' => 'ts', 'type' => \Swoole\Table::TYPE_FLOAT],
+            ],
+        ],
+        'eventOddsHash'             => [
+            'size'   => 2000,
+            'column' => [
+                ['name' => 'hash', 'type' => \Swoole\Table::TYPE_STRING, "size" => 37],
+            ],
+        ],
+        'leagues'                   => [
+            'size'   => 10000, //@TODO needs to be adjusted once additional provider comes in
+            'column' => [
+                ['name' => 'name', 'type' => \Swoole\Table::TYPE_STRING, "size" => 80],
+                ['name' => 'sport_id', 'type' => \Swoole\Table::TYPE_INT],
+                ['name' => 'provider_id', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'leaguesIndex'              => [
+            'size'   => 10000, //@TODO needs to be adjusted once additional provider comes in
+            'column' => [
+                ['name' => 'league_id', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'masterLeagues'             => [
+            'size'   => 10000,
+            'column' => [
+                ['name' => 'name', 'type' => \Swoole\Table::TYPE_STRING, "size" => 80],
+                ['name' => 'sport_id', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'masterLeaguesIndex'        => [ // md5($leagueName . ':' . $sportId)
+                                         'size'   => 10000,
+                                         'column' => [
+                                             ['name' => 'master_league_id', 'type' => \Swoole\Table::TYPE_INT],
+                                         ],
+        ],
+        'teams'                     => [
+            'size'   => 20000, //@TODO needs to be adjusted once additional provider comes in
+            'column' => [
+                ['name' => 'name', 'type' => \Swoole\Table::TYPE_STRING, "size" => 80],
+                ['name' => 'sport_id', 'type' => \Swoole\Table::TYPE_INT],
+                ['name' => 'provider_id', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'teamsIndex'                => [
+            'size'   => 20000, //@TODO needs to be adjusted once additional provider comes in
+            'column' => [
+                ['name' => 'team_id', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'masterTeams'               => [
+            'size'   => 20000,
+            'column' => [
+                ['name' => 'name', 'type' => \Swoole\Table::TYPE_STRING, "size" => 80],
+                ['name' => 'sport_id', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'masterTeamsIndex'          => [
+            'size'   => 20000,
+            'column' => [
+                ['name' => 'master_team_id', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'enabledSports'             => [
+            'size'   => 100,
+            'column' => [
+                ['name' => 'value', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        'enabledProviders'          => [
+            'size'   => 100,
+            'column' => [
+                ['name' => 'value', 'type' => \Swoole\Table::TYPE_FLOAT],
+                ['name' => 'currencyId', 'type' => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        "events"                    => [
+            "size"   => 10000,
+            "column" => [
+                ["name" => "sport", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "schedule", "type" => \Swoole\Table::TYPE_STRING, "size" => 6],
+                ["name" => "provider", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "missingCount", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "league_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "team_home_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "team_away_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "master_event_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "ref_schedule", "type" => \Swoole\Table::TYPE_STRING, "size" => 20]
+            ],
+        ],
+        "eventsIndex"               => [
+            "size"   => 10000,
+            "column" => [
+                ["name" => "event_identifier", "type" => \Swoole\Table::TYPE_STRING, "size" => 30]
+            ],
+        ],
+        "eventsIndexKeys"           => [
+            "size"   => 10000,
+            "column" => [
+                ["name" => "eventIndexHash", "type" => \Swoole\Table::TYPE_STRING, "size" => 32]
+            ],
+        ],
+        "masterEvents"              => [
+            "size"   => 10000,
+            "column" => [
+                ["name" => "master_event_unique_id", "type" => \Swoole\Table::TYPE_STRING, "size" => 30],
+                ["name" => "sport_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "ref_schedule", "type" => \Swoole\Table::TYPE_STRING, "size" => 20],
+                ["name" => "master_league_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "master_team_home_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "master_team_away_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "game_schedule", "type" => \Swoole\Table::TYPE_STRING, "size" => 6],
+                ["name" => "score", "type" => \Swoole\Table::TYPE_STRING, "size" => 7],
+                ["name" => "running_time", "type" => \Swoole\Table::TYPE_STRING, "size" => 15],
+                ["name" => "home_penalty", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "home_penalty", "type" => \Swoole\Table::TYPE_INT],
+            ],
+        ],
+        "masterEventsIndex"         => [
+            "size"   => 10000,
+            "column" => [
+                ["name" => "master_event_id", "type" => \Swoole\Table::TYPE_INT]
+            ],
+        ],
+        "masterEventsIndexKeys"     => [
+            "size"   => 10000,
+            "column" => [
+                ["name" => "masterEventIndexHash", "type" => \Swoole\Table::TYPE_STRING, "size" => 32]
+            ],
+        ],
+        "eventScores"               => [
+            "size"   => 10000,
+            "column" => [
+                ["name" => "value", "type" => \Swoole\Table::TYPE_INT]
+            ],
+        ],
+        "eventMarkets"              => [
+            "size"   => 100000,
+            "column" => [
+                ["name" => "master_event_market_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "bet_identifier", "type" => \Swoole\Table::TYPE_STRING, "size" => 20],
+                ["name" => "event_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "provider_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "odd_type_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "market_event_identifier", "type" => \Swoole\Table::TYPE_STRING, "size" => 30],
+                ["name" => "market_flag", "type" => \Swoole\Table::TYPE_STRING, "size" => 5],
+                ["name" => "is_main", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "odd_label", "type" => \Swoole\Table::TYPE_STRING, "size" => 10],
+                ["name" => "odds", "type" => \Swoole\Table::TYPE_FLOAT],
+            ],
+        ],
+        "eventMarketsIndex"         => [
+            "size"   => 100000,
+            "column" => [
+                ["name" => "bet_identifier", "type" => \Swoole\Table::TYPE_STRING, "size" => 20]
+            ],
+        ],
+        "eventMarketList"           => [
+            "size"   => 20000,
+            "column" => [
+                ["name" => "marketIDs", "type" => \Swoole\Table::TYPE_STRING, "size" => 3000]
+            ],
+        ],
+        "masterEventMarkets"        => [
+            "size"   => 100000,
+            "column" => [
+                ["name" => "is_main", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "master_event_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "master_event_market_unique_id", "type" => \Swoole\Table::TYPE_STRING, "size" => 32]
+            ],
+        ],
+        "masterEventMarketsIndex"   => [
+            "size"   => 100000,
+            "column" => [
+                ["name" => "master_event_market_id", "type" => \Swoole\Table::TYPE_INT]
+            ],
+        ],
+        "masterEventMarketList"     => [
+            "size"   => 20000,
+            "column" => [
+                ["name" => "memUIDs", "type" => \Swoole\Table::TYPE_STRING, "size" => 3000]
+            ],
+        ],
+        "sportsOddTypes"            => [
+            "size"   => 50,
+            "column" => [
+                ["name" => "value", "type" => \Swoole\Table::TYPE_INT]
+            ],
+        ],
+        "activeOrders"              => [
+            "size"   => 1000,
+            "column" => [
+                ["name" => "createdAt", "type" => \Swoole\Table::TYPE_STRING, "size" => 50],
+                ["name" => "betId", "type" => \Swoole\Table::TYPE_STRING, "size" => 30],
+                ["name" => "orderExpiry", "type" => \Swoole\Table::TYPE_STRING, "size" => 3],
+                ["name" => "username", "type" => \Swoole\Table::TYPE_STRING, "size" => 20],
+                ["name" => "userCurrencyId", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "status", "type" => \Swoole\Table::TYPE_STRING, "size" => 20],
+            ],
+        ],
+        "statsCountEventsPerSecond" => [
+            "size"   => 400,
+            "column" => [
+                ["name" => "total", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "processed", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "error", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveSport", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveProvider", "type" => \Swoole\Table::TYPE_FLOAT],
+            ]
+        ],
+        "statsTimeEventsPerSecond"  => [
+            "size"   => 400,
+            "column" => [
+                ["name" => "total", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "processed", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "error", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveSport", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveProvider", "type" => \Swoole\Table::TYPE_FLOAT],
+            ]
+        ],
+        "statsCountOddsPerSecond"   => [
+            "size"   => 400,
+            "column" => [
+                ["name" => "total", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "processed", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "error", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveSport", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveProvider", "type" => \Swoole\Table::TYPE_FLOAT],
+            ]
+        ],
+        "statsTimeOddsPerSecond"    => [
+            "size"   => 400,
+            "column" => [
+                ["name" => "total", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "processed", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "error", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveSport", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveProvider", "type" => \Swoole\Table::TYPE_FLOAT],
+            ]
+        ],
+        "statsCountOddsPerSecond"   => [
+            "size"   => 400,
+            "column" => [
+                ["name" => "total", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "processed", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "error", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveSport", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveProvider", "type" => \Swoole\Table::TYPE_FLOAT],
+            ]
+        ],
+        "statsTimeOddsPerSecond"    => [
+            "size"   => 400,
+            "column" => [
+                ["name" => "total", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "processed", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "error", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveSport", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "inactiveProvider", "type" => \Swoole\Table::TYPE_FLOAT],
+            ]
+        ],
+        "requestTimers"             => [
+            "size"   => 5,
+            "column" => [
+                ["name" => "value", "type" => \Swoole\Table::TYPE_INT],
+            ]
+        ],
+        "requestIntervals"          => [
+            "size"   => 4,
+            "column" => [
+                ["name" => "value", "type" => \Swoole\Table::TYPE_INT],
+            ]
+        ],
+        "providerAccounts"          => [
+            "size"   => 100,
+            "column" => [
+                ["name" => "provider_id", "type" => \Swoole\Table::TYPE_INT],
+                ["name" => "username", "type" => \Swoole\Table::TYPE_STRING, "size" => 32],
+                ["name" => "punter_percentage", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "credits", "type" => \Swoole\Table::TYPE_FLOAT],
+                ["name" => "alias", "type" => \Swoole\Table::TYPE_STRING, "size" => 16],
+                ['name' => 'type', 'type' => \Swoole\Table::TYPE_STRING, "size" => 50],
+            ]
+        ],
+        'maintenance'               => [
+            "size"   => 64,
+            "column" => [
+                ["name" => "under_maintenance", "type" => \Swoole\Table::TYPE_STRING, 'size' => 5],
+            ]
+        ],
+        'walletClients'               => [
+            "size"   => 64,
+            "column" => [
+                ["name" => "token", "type" => \Swoole\Table::TYPE_STRING, 'size' => 32],
+            ]
+        ],
+    ],
+];
