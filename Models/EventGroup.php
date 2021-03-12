a56d707d557c92906958c11210ef278203ffb7ec
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2021-03-12 16:37:19 +0800

    - Connection and logic update

--- Models/EventGroup.php
+++ Models/EventGroup.php
@@ -19,14 +19,12 @@ class EventGroup extends Model
         $sql = "SELECT * FROM " . static::$table . " 
                 JOIN events ON events.id = " . static::$table . ".event_id WHERE " . static::$table . ".master_event_id = '{$masterEventId}' AND deleted_at is null";
 
-                // var_dump($sql);
         return $connection->query($sql);
     }
 
     public static function getDataByEventId($connection, $eventId)
     {
         $sql = "SELECT * FROM " . self::$table . " WHERE event_id = '{$eventId}' LIMIT 1";
-        // var_dump($sql);
         return $connection->query($sql);
     }
 }
