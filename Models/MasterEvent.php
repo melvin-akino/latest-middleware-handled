1d7d9ec01b5a094c2bdb2136e5abdf89eb416e5f
Melvin Aquino <melvinaquino@ninepinetech.com>
2021-03-16 15:18:01 +0800

    Added reference schedule in the master events query condition to eliminate the if condition in the matching logic

--- Models/MasterEvent.php
+++ Models/MasterEvent.php
@@ -8,12 +8,16 @@ class MasterEvent extends Model
 {
     protected static $table = 'master_events';
 
-    public static function getMasterEventData($connection, $masterLeagueId, $masterHomeTeamId, $masterAwayTeamId)
+    public static function getMasterEventData($connection, $masterLeagueId, $masterHomeTeamId, $masterAwayTeamId, $refSchedule)
     {
         $sql = "SELECT * FROM " . static::$table 
                 . " JOIN event_groups ON " . static::$table . ".id = event_groups.master_event_id"
                 . " JOIN events ON event_groups.event_id = events.id"
-                . " WHERE master_league_id = '{$masterLeagueId}' AND master_home_team_id = '{$masterHomeTeamId}' AND master_away_team_id = '{$masterAwayTeamId}' AND deleted_at IS NULL ORDER BY created_at DESC LIMIT 1";
+                . " WHERE master_league_id = '{$masterLeagueId}'"
+                . " AND master_team_home_id = '{$masterHomeTeamId}'"
+                . " AND master_team_away_id = '{$masterAwayTeamId}'" 
+                . " AND " . static::$table . ".ref_schedule = '{$refSchedule}'"
+                . " AND " . static::$table . ".deleted_at IS NULL ORDER BY created_at DESC LIMIT 1";
         return $connection->query($sql);
     }
 }
