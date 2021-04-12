e795eb5ff79d5d6335505f386537307a0f132cf8
Xander <xander@Jans-MacBook-Pro.local>
2021-04-12 11:05:44 +0800

    - added valiation if model response has result

--- Workers/ProcessEvent.php
+++ Workers/ProcessEvent.php
@@ -102,20 +102,24 @@ class ProcessEvent
                                 $eventId = $myEvent['id'];
 
                                 $myMasterEventResult = EventGroup::getDataByEventId($connection, $eventId);
-                                $myMasterEvent = $connection->fetchAssoc($myMasterEventResult);
+                                if ($connection->numRows($myMasterEventResult) > 0) {
+                                    $myMasterEvent = $connection->fetchAssoc($myMasterEventResult);
 
-                                $myMatchedEventsResult = EventGroup::getMatchedEvents($connection, $myMasterEvent['master_event_id'], $eventId);
-                                $myMatchedEvents = $connection->fetchAll($myMatchedEventsResult);
+                                    $myMatchedEventsResult = EventGroup::getMatchedEvents($connection, $myMasterEvent['master_event_id'], $eventId);
+                                    $myMatchedEvents = $connection->fetchAll($myMatchedEventsResult);
 
-                                foreach ($myMatchedEvents as $matchedEvent) {
-                                    UnmatchedData::create($connection, [
-                                        'provider_id' => $providerId,
-                                        'data_type' => 'event',
-                                        'data_id' => $matchedEvent['event_id']
-                                    ]);
+                                    if (is_array($myMatchedEvents)) {
+                                        foreach ($myMatchedEvents as $matchedEvent) {
+                                            UnmatchedData::create($connection, [
+                                                'provider_id' => $providerId,
+                                                'data_type' => 'event',
+                                                'data_id' => $matchedEvent['event_id']
+                                            ]);
+                                        }
+        
+                                        EventGroup::deleteMatchesOfEvent($connection, $myMasterEvent['master_event_id'], $eventId);
+                                    }
                                 }
-
-                                EventGroup::deleteMatchesOfEvent($connection, $myMasterEvent['master_event_id'], $eventId);
 
                                 Event::update($connection, [
                                     'deleted_at' => Carbon::now()
@@ -130,12 +134,15 @@ class ProcessEvent
                                     if (!empty($marketId)) {
 
                                         $myEventMarketResult = EventMarket::getDataByBetIdentifier($connection, $marketId);
-                                        $myEventMarket = $connection->fetchAssoc($myEventMarketResult);
+                                        if ($connection->numRows($myEventMarketResult)) {
+                                            $myEventMarket = $connection->fetchAssoc($myEventMarketResult);
 
-                                        $myEventMarketGroupResult = EventMarketGroup::getDataByEventMarketId($connection, $myEventMarket['id']);
-                                        $myEventMarketGroup = $connection->fetchAssoc($myEventMarketGroupResult);
-
-                                        EventMarketGroup::deleteMatchesOfEventMarket($connection, $myEventMarketGroup['master_event_market_id']);
+                                            $myEventMarketGroupResult = EventMarketGroup::getDataByEventMarketId($connection, $myEventMarket['id']);
+                                            if ($connection->numRows($myEventMarketGroupResult)) {
+                                                $myEventMarketGroup = $connection->fetchAssoc($myEventMarketGroupResult);
+                                                EventMarketGroup::deleteMatchesOfEventMarket($connection, $myEventMarketGroup['master_event_market_id']);
+                                            }
+                                        }
 
                                         EventMarket::update($connection, [
                                             'deleted_at' => Carbon::now()
