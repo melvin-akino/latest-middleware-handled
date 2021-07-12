b938d215b5007f690fd4a3a7e7c68c34db511777
Kevin Uy <kevinuy@ninepinetech.com>
2021-07-12 20:17:39 +0800

    Revert "HOTFIX-CORO-SESSION-MANAGEMENT-FIX"
    This reverts commit 014ee85595c674c97b2b5e739b4a1e91bb8d60b9.

--- Services/SessionKafkaReactor.php
+++ Services/SessionKafkaReactor.php
@@ -96,7 +96,7 @@ function sessionHandler($message, $offset)
     global $dbPool;
 
     try {
-        $previousTS = $swooleTable['timestamps']['sessions:' . $message['sub_command'] . ':' . strtolower($message['data']['provider'])]['ts'];
+        $previousTS = $swooleTable['timestamps']['sessions:' . $message['sub_command']]['ts'];
         $messageTS  = $message["request_ts"];
         if ($messageTS < $previousTS) {
             logger('info','sessions-reactor', 'Validation Error: Timestamp is old', (array) $message);
